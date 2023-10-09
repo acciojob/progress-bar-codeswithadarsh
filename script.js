b708729ae3ecf70064dd65e175919eb298b278ba
Adarsh Pandey <97337633+codeswithadarsh@users.noreply.github.com>
2023-10-09 09:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
-//your JS code here. If required.
+document.addEventListener('DOMContentLoaded', function () {
+    const circles = document.querySelectorAll('.circle');
+    const prevButton = document.getElementById('prev');
+    const nextButton = document.getElementById('next');
+    let currentStep = 1;
+
+    nextButton.addEventListener('click', function () {
+        if (currentStep < circles.length) {
+            circles[currentStep - 1].classList.remove('active');
+            circles[currentStep].classList.add('active');
+            currentStep++;
+            updateButtonState();
+        }
+    });
+
+    prevButton.addEventListener('click', function () {
+        if (currentStep > 1) {
+            currentStep--;
+            circles[currentStep].classList.remove('active');
+            circles[currentStep - 1].classList.add('active');
+            updateButtonState();
+        }
+    });
+
+    function updateButtonState() {
+        prevButton.disabled = currentStep === 1;
+        nextButton.disabled = currentStep === circles.length;
+    }
+});
